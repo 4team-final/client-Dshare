8378a866620da9bcf08de9cfa40c576b0d2af6d4
woohyeoung <dngud951108@gmail.com>
2022-07-01 11:22:47 +0900

    feat: 타임테이블 수정 추가 #21

--- src/components/TimeTable/TimeTableService.js
+++ src/components/TimeTable/TimeTableService.js
@@ -1,25 +1,34 @@
+import { CardFrame } from "./TimeTableStyle";
 import { Card, List } from "antd";
+import "antd/dist/antd.css";
 const data = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ];
 
 export const TimeTableContent = () => {
   return (
     <List
       grid={{
-        gutter: 16,
-        xs: 1,
-        sm: 2,
-        md: 4,
-        lg: 4,
-        xl: 6,
-        xxl: 24,
+        gutter: 4,
+        xs: 48,
+        sm: 48,
+        md: 48,
+        lg: 48,
+        xl: 48,
+        xxl: 48,
       }}
       dataSource={data}
-      renderItem={(item) => (
+      renderItem={(item, i) => (
         <List.Item>
-          <Card title={item.title}></Card>
+          <CardFrame>
+            <Card
+              style={{
+                height: `${i === 0 || i === 47 ? 50 : i % 2 === 0 ? 35 : 25}px`,
+              }}
+              bodyStyle={{ backgroundColor: "#1296ec" }}
+            />
+          </CardFrame>
         </List.Item>
       )}
     />
--- src/components/TimeTable/TimeTableStyle.js
+++ src/components/TimeTable/TimeTableStyle.js
@@ -3,38 +3,27 @@ import styled from "styled-components";
 export const FullWidthFrame = styled.div`
   max-width: 1000px;
   height: 100px;
-  background-color: #1296ec;
+  background-color: #fff;
   text-align: center;
   margin: 15px auto;
   border-radius: 30px;
+  border: #1296ec 1px solid;
+  display: flex;
 `;
 
 export const ComponentFrame = styled.div`
   width: 97%;
   height: auto;
-  overflow-x: scroll;
   overflow-y: none;
-  margin: 0 auto;
+  margin: auto;
   padding-top: 1%;
   padding-bottom: 3%;
 `;
 
 export const CardFrame = styled.div`
-  margin: 0;
+  margin: auto;
   padding: 0;
-  width: 30px;
-  height: 50px;
-`;
-
-export const List = styled.ul`
-  list-style: none;
-  margin: 0;
-  padding: 0;
-`;
-
-export const ListItem = styled.li`
-  margin: 0;
-  padding: 0;
-  border: 0;
-  display: inline-block;
+  width: 5px;
+  height: ${(i) => (i === 0 || i === 47 ? 50 : i % 2 === 0 ? 35 : 20)}px;
+  background-color: #fff;
 `;
