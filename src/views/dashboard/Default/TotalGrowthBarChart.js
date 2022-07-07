65c2fee968fbd930e008cac1bb203504f3b69cc9
jaebin1234 <jaebin1234>
2022-07-07 19:14:09 +0900

    feat: 메인페이지 통계 완료

--- src/views/dashboard/Default/TotalGrowthBarChart.js
+++ src/views/dashboard/Default/TotalGrowthBarChart.js
@@ -61,7 +61,7 @@ const status = [
     },
     {
         value: '3',
-        label: '사람들이 이용하는 시간대'
+        label: '사람들이 이용을 시작하는 시간대'
     }
 ];
 
@@ -183,7 +183,6 @@ const TotalGrowthBarChart = ({ isLoading }) => {
     // 차량 /  회의실 선택 => 회의실 0, 차량 1
     useEffect(() => {
         if (changeStoreSelect == 0 || changeStoreSelect == 1) {
-            console.log(changeStoreSelect);
             isSelect(changeStoreSelect);
         }
     }, [changeStoreSelect]);
@@ -191,7 +190,6 @@ const TotalGrowthBarChart = ({ isLoading }) => {
     // 통계 3개 선택  => 1,2,3
     useEffect(() => {
         if (changeStoreSelect2 >= 1 && changeStoreSelect2 <= 3) {
-            console.log(changeStoreSelect2);
             isSelect2(changeStoreSelect2);
         }
     }, [changeStoreSelect2]);
@@ -250,8 +248,6 @@ const TotalGrowthBarChart = ({ isLoading }) => {
     //1,3,7 일간 많이 예약된 차량
     useEffect(() => {
         if (oneDayVehicleReservaionCount?.data?.value) {
-            console.log(oneDayVehicleReservaionCount);
-
             setOneDayList4(oneDayVehicleReservaionCount.data.value.sort((a, b) => sort1(a.vehicle.id, b.vehicle.id)));
         }
         if (threeDayVehicleReservationCount?.data?.value) {
@@ -265,7 +261,7 @@ const TotalGrowthBarChart = ({ isLoading }) => {
     //1,3,7 일간 많이 예약한 차량 시간대
     useEffect(() => {
         if (oneDayVehicleReservaionTime?.data?.value) {
-            console.log(oneDayVehicleReservaionTime);
+            console.log(oneDayVehicleReservaionCount);
             setOneDayList5(oneDayVehicleReservaionTime.data.value.sort((a, b) => sort1(a.id, b.id)));
         }
         if (threeDayVehicleReservationTime?.data?.value) {
@@ -279,8 +275,6 @@ const TotalGrowthBarChart = ({ isLoading }) => {
     //1,3,7 일간 많이 차량을 이용 시작 시간대
     useEffect(() => {
         if (oneDayVehicleStartTime?.data?.value) {
-            console.log(oneDayVehicleStartTime);
-
             setOneDayList6(oneDayVehicleStartTime.data.value.sort((a, b) => sort1(a.reservationId, b.reservationId)));
         }
         if (threeDayVehicleStartTime?.data?.value) {
@@ -294,8 +288,12 @@ const TotalGrowthBarChart = ({ isLoading }) => {
     useEffect(() => {
         // 첫번째 회의실 리스트 렌더링
         if (select == 0 && select2 == 1 && onedayList1.length > 0 && threedayList1.length > 0 && sevendayList1.length > 0) {
-            categoriesName(onedayList1, threedayList1, sevendayList1);
-            dataCount(onedayList1, threedayList1, sevendayList1);
+            async function fetch() {
+                const List = await categoriesName(onedayList1, threedayList1, sevendayList1);
+                setArrX(List);
+                await dataCount(onedayList1, threedayList1, sevendayList1, List);
+            }
+            fetch();
         }
     }, [onedayList1, threedayList1, sevendayList1, select, select2]);
 
@@ -314,7 +312,7 @@ const TotalGrowthBarChart = ({ isLoading }) => {
     useEffect(() => {
         if (select == 0 && select2 == 3 && onedayList3.length > 0 && threedayList3.length > 0 && sevendayList3.length > 0) {
             async function fetch() {
-                const List = categoriesTime(onedayList3, threedayList3, sevendayList3);
+                const List = await categoriesTime(onedayList3, threedayList3, sevendayList3);
                 setArrX(List);
                 await dataTimeCount(onedayList3, threedayList3, sevendayList3, List);
             }
@@ -323,24 +321,32 @@ const TotalGrowthBarChart = ({ isLoading }) => {
     }, [onedayList3, threedayList3, sevendayList3, select, select2]);
     useEffect(() => {
         if (select == 1 && select2 == 1 && onedayList4.length > 0 && threedayList4.length > 0 && sevendayList4.length > 0) {
-            console.log(select);
-            console.log(select2);
-            categoriesName(onedayList4, threedayList4, sevendayList4);
-            dataCount(onedayList3, threedayList3, sevendayList3);
+            async function fetch() {
+                const List = await categoriesName(onedayList4, threedayList4, sevendayList4);
+                setArrX(List);
+                await dataCount(onedayList4, threedayList4, sevendayList4, List);
+            }
+            fetch();
         }
     }, [onedayList4, threedayList4, sevendayList4, select, select2]);
     useEffect(() => {
         if (select == 1 && select2 == 2 && onedayList5.length > 0 && threedayList5.length > 0 && sevendayList5.length > 0) {
-            console.log(select);
-            console.log(select2);
-            categoriesTime(onedayList5, threedayList5, sevendayList5);
-            dataCount(onedayList3, threedayList3, sevendayList3);
+            async function fetch() {
+                const List = await categoriesTime(onedayList5, threedayList5, sevendayList5);
+                setArrX(List);
+                await dataTimeCount(onedayList5, threedayList5, sevendayList5, List);
+            }
+            fetch();
         }
     }, [onedayList5, threedayList5, sevendayList5, select, select2]);
     useEffect(() => {
         if (select == 1 && select2 == 3 && threedayList6.length > 0 && sevendayList6.length > 0 && sevendayList6.length > 0) {
-            categoriesTime(onedayList6, threedayList6, sevendayList6);
-            dataCount(onedayList6, threedayList6, sevendayList6);
+            async function fetch() {
+                const List = await categoriesTime(onedayList6, threedayList6, sevendayList6);
+                setArrX(List);
+                await dataTimeCount(onedayList6, threedayList6, sevendayList6, List);
+            }
+            fetch();
         }
     }, [onedayList6, threedayList6, sevendayList6, select, select2]);
 
@@ -361,20 +367,21 @@ const TotalGrowthBarChart = ({ isLoading }) => {
         copyList = copyList.filter((item, i) => copyList.indexOf(item) === i);
 
         copyList.sort((a, b) => sort1(a, b));
-        setArrX(copyList);
+        return copyList;
+        // setArrX(copyList);
     }, []);
     // x축 카테고리 설정 - 시간
     const categoriesTime = useCallback((onedayList11, threedayList11, sevendayList11) => {
         let copyList = [];
 
         onedayList11.map((item) => {
-            copyList.push(item?.hour);
+            copyList.push(item?.hour || item?.htime);
         });
         threedayList11.map((item) => {
-            copyList.push(item?.hour);
+            copyList.push(item?.hour || item?.htime);
         });
         sevendayList11.map((item) => {
-            copyList.push(item?.hour);
+            copyList.push(item?.hour || item?.htime);
         });
 
         copyList = copyList.filter((item, i) => copyList.indexOf(item) === i);
@@ -384,24 +391,48 @@ const TotalGrowthBarChart = ({ isLoading }) => {
         // setArrX(copyList);
     }, []);
 
-    // y축 데이터 - 회의실 별 개수
-    const dataCount = useCallback((onedayList11, threedayList11, sevendayList11) => {
-        console.log(onedayList1);
-        console.log(threedayList1);
-        console.log(sevendayList1);
-        let oneList = [];
-        let threeList = [];
-        let sevenList = [];
+    // y축 데이터 - 회의실/차량 별 개수
+    const dataCount = useCallback((onedayList11, threedayList11, sevendayList11, List) => {
+        let oneList = [...List];
+        oneList.fill(0);
+        let threeList = [...List];
+        threeList.fill(0);
+        let sevenList = [...List];
+        sevenList.fill(0);
 
         onedayList11.map((item) => {
-            oneList.push(item?.count || item?.vcount || item?.hcount);
+            let key = List.findIndex((i) => i == (item?.roomNo || item?.vehicle?.name));
+
+            if (key != -1) {
+                // 원래 있던 갯수
+                let beforeCount = oneList[key];
+                // 이번 개수
+                let Count = item?.count || item?.vcount;
+                oneList.splice(key, 1, beforeCount + Count);
+            }
         });
         threedayList11.map((item) => {
-            threeList.push(item?.count || item?.vcount || item?.hcount);
+            let key = List.findIndex((i) => i == (item?.roomNo || item?.vehicle?.name));
+
+            if (key != -1) {
+                // 원래 있던 갯수
+                let beforeCount = threeList[key];
+                // 이번 개수
+                let Count = item?.count || item?.vcount;
+                threeList.splice(key, 1, beforeCount + Count);
+            }
         });
         sevendayList11.map((item) => {
-            sevenList.push(item?.count || item?.vcount || item?.hcount);
+            let key = List.findIndex((i) => i == (item?.roomNo || item?.vehicle?.name));
+            if (key != -1) {
+                // 원래 있던 갯수
+                let beforeCount = sevenList[key];
+                // 이번 개수
+                let Count = item?.count || item?.vcount;
+                sevenList.splice(key, 1, beforeCount + Count);
+            }
         });
+
         setOneData(oneList);
         setThreeData(threeList);
         setSevenData(sevenList);
@@ -426,7 +457,7 @@ const TotalGrowthBarChart = ({ isLoading }) => {
         console.log(sevenList);
 
         for (let i = 0; i < onedayList11.length; i++) {
-            let item = onedayList11[i].hour;
+            let item = onedayList11[i]?.hour || onedayList11[i]?.htime;
 
             // 몇시인지 인덱스를 찾음
             let key = tempX.findIndex((i) => i == item);
@@ -434,31 +465,31 @@ const TotalGrowthBarChart = ({ isLoading }) => {
                 // 원래 있던 갯수
                 let beforeCount = oneList[key];
                 // 이번 개수
-                let Count = onedayList11[i].count;
+                let Count = onedayList11[i]?.count || onedayList11[i]?.hcount;
                 oneList.splice(key, 1, beforeCount + Count);
             }
         }
         for (let i = 0; i < threedayList11.length; i++) {
-            let item = threedayList11[i].hour;
+            let item = threedayList11[i]?.hour || threedayList11[i]?.htime;
             // 몇시인지 인덱스를 찾음
             let key = tempX.findIndex((i) => i == item);
             if (key != -1) {
                 // 원래 있던 갯수
                 let beforeCount = threeList[key];
                 // 이번 개수
-                let Count = threedayList11[i].count;
+                let Count = threedayList11[i]?.count || threedayList11[i]?.hcount;
                 threeList.splice(key, 1, beforeCount + Count);
             }
         }
         for (let i = 0; i < sevendayList11.length; i++) {
-            let item = sevendayList11[i].hour;
+            let item = sevendayList11[i]?.hour || sevendayList11[i]?.htime;
             // 몇시인지 인덱스를 찾음
             let key = tempX.findIndex((i) => i == item);
             if (key != -1) {
                 // 원래 있던 갯수
                 let beforeCount = sevenList[key];
                 // 이번 개수
-                let Count = sevendayList11[i].count;
+                let Count = sevendayList11[i]?.count || sevendayList11[i]?.hcount;
                 sevenList.splice(key, 1, beforeCount + Count);
             }
         }
@@ -505,6 +536,7 @@ const TotalGrowthBarChart = ({ isLoading }) => {
     const handleChange = (event) => {
         setGraphType(event.target.value);
     };
+    const [tempData, setTempData] = useState({});
     useEffect(() => {
         if (graphType === 'bar') {
             setchartDataResult({
@@ -540,30 +572,6 @@ const TotalGrowthBarChart = ({ isLoading }) => {
                     ...chartData2.options,
                     xaxis: {
                         ...chartDataResult.options.xaxis,
-                        categories: chartDataResult.options.xaxis.categories
-                    }
-                },
-                series: [
-                    {
-                        name: '1일',
-                        data: oneData
-                    },
-                    {
-                        name: '3일',
-                        data: threeData
-                    },
-                    {
-                        name: '7일',
-                        data: sevenData
-                    }
-                ]
-            });
-        } else if (graphType === 'line') {
-            setchartDataResult({
-                type: 'line',
-                options: {
-                    ...chartData3.options,
-                    xaxis: {
                         categories: chartDataResult.options.xaxis.categories
                     }
                 },
@@ -675,7 +683,6 @@ const TotalGrowthBarChart = ({ isLoading }) => {
                                                     >
                                                         <MenuItem value={'bar'}>bar</MenuItem>
                                                         <MenuItem value={'heatmap'}>heatmap</MenuItem>
-                                                        <MenuItem value={'line'}>line</MenuItem>
                                                     </Select>
                                                 </FormControl>
                                             </Box>
